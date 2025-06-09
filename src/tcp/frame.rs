2764c656a9a051ce9b7dfe0b6ec819196c8b97c2
tsabelmann <tim.sabelmann@hotmail.de>
2025-06-09 15:00:38 +0200

    feat: Add skeleton for ModbusTcpFrame

--- src/tcp/frame.rs
+++ src/tcp/frame.rs
@@ -1,25 +1,39 @@
-///
-pub const MODBUS_TCP_FRAME_DATA_SIZE: usize = 253;
+pub const MODBUS_FRAME_DATA_LENTGH: usize = 260;
 
-pub struct ModbusTcpFrame {
-    transaction_identifier: u16,
-    protocol_identifier: u16,
-    unit_identifier: u8,
-    function_code: u8,
-    data: [u8; MODBUS_TCP_FRAME_DATA_SIZE],
-    data_length: usize,
+
+
+pub struct ModbusTcpFrame<'a> {
+    data: &'a mut [u8; MODBUS_FRAME_DATA_LENTGH],
+    data_length: usize
 }
 
-impl ModbusTcpFrame {
-    pub const fn new(transaction_identifier: u16, unit_identifier: u8, function_code: u8) -> ModbusTcpFrame {
-        ModbusTcpFrame {
-            transaction_identifier,
-            protocol_identifier: 0,
-            unit_identifier,
-            function_code,
-            data: [0u8; MODBUS_TCP_FRAME_DATA_SIZE],
-            data_length: 0
-        }
+impl<'a> ModbusTcpFrame<'a> {
+    pub unsafe fn new_unchecked(data: &'a mut [u8; MODBUS_FRAME_DATA_LENTGH], data_length: usize) -> ModbusTcpFrame<'a> {
+        ModbusTcpFrame { data, data_length:  data_length.min(MODBUS_FRAME_DATA_LENTGH) }
+    }
+
+    pub const fn transaction_identifier(&self) -> u16 {
+        let data = [self.data[0], self.data[1]];
+        u16::from_be_bytes(data)
+    }
+
+    pub const fn protocol_identifier(&self) -> u16 {
+        let data = [self.data[2], self.data[3]];
+        u16::from_be_bytes(data)
+    }
+
+    pub const fn length(&self) -> u16 {
+        let data = [self.data[4], self.data[5]];
+        u16::from_be_bytes(data)
+    }
+
+    pub const fn unit_identifier(&self) -> u8 {
+        self.data[6]
+    }
+
+    pub const fn is_valid(&self) -> bool {
+        let length = 6 + self.length() as usize;
+        length == self.data.len()
     }
 }
 
