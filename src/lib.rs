f74e6b0510f09ebb345afb9c98d72330ca202f80
tsabelmann <tim.sabelmann@hotmail.de>
2025-06-10 16:00:19 +0200

    feat: Add global import for pdu traits, and server submodule

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,20 @@ extern crate std;
 /// Register related types and helper functions.
 pub mod register;
 
+/// Function code related types.
+mod codes;
+pub use codes::{FunctionCode, FunctionKind};
+
 /// Exception code related types and helper functions.
 mod exception;
 pub use exception::ExceptionCode;
 
+/// PDU related types.
+mod pdu;
+pub use pdu::{PduData, PduDataMut};
+
 /// TCP/IP related types and helper functions.
 pub mod tcp;
+
+/// Server related types and helper functions.
+pub mod server;
