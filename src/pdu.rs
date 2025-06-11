88ae82cd91c3ccb4a606d68a92f07e20712d726f
tsabelmann <tim.sabelmann@hotmail.de>
2025-06-11 11:28:56 +0200

    feat: Add methods length, and set_length to PduData and PduDataMut

--- src/pdu.rs
+++ src/pdu.rs
@@ -3,9 +3,11 @@ use crate::FunctionKind;
 pub trait PduData {
     fn pdu_data(&self) -> & [u8];
     fn function_code(&self) -> FunctionKind;
+    fn length(&self) -> Option<u16>;
 }
 
 pub trait PduDataMut : PduData {
     fn pdu_data_mut(&mut self) -> &mut [u8];
     fn set_function_code(&mut self, code: FunctionKind);
+    fn set_length(&mut self, length: u16);
 }
