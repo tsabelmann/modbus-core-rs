cb36dcf9dd5a4076349f7441de11670e9c2a300d
tsabelmann <tim.sabelmann@hotmail.de>
2025-09-29 12:50:32 +0200

    feat: Add new constant for modbus RTU frames

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,5 @@
-
 /// Maximum number of bytes in a modbus TCP/IP frame.
 pub const MODBUS_TCP_FRAME_DATA_LENTGH: usize = 260;
+
+/// Maximum number of bytes in a modbus RTU frame.
+pub const MODBUS_RTU_FRAME_DATA_LENGTH: usize = 256;
