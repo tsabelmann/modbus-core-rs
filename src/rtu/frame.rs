82839ad6379ad4b463960e751993800c63778035
tsabelmann <tim.sabelmann@hotmail.de>
2025-09-29 13:25:50 +0200

    fix: Correct array boundaries

--- src/rtu/frame.rs
+++ src/rtu/frame.rs
@@ -2,20 +2,33 @@ use crate::{FunctionKind, PduData, PduDataMut};
 use crate::constants::MODBUS_RTU_FRAME_DATA_LENGTH;
 
 
-
 pub struct ModbusRtuFrame<'a> {
     data: &'a mut [u8; MODBUS_RTU_FRAME_DATA_LENGTH]
 }
 
+impl<'a> ModbusRtuFrame<'a> {
+    pub fn slave_address(&self) -> u8 {
+        self.data[0]
+    }
+
+    pub fn checksum(&self) -> u16 {
+        let data = [
+            self.data[MODBUS_RTU_FRAME_DATA_LENGTH-2],
+            self.data[MODBUS_RTU_FRAME_DATA_LENGTH-1]
+        ];
+        let crc = u16::from_le_bytes(data);
+        crc
+    }
+}
 
 impl<'a> PduData for ModbusRtuFrame<'a> {
     fn pdu_data(&self) -> & [u8] {
         const END_INDEX: usize = MODBUS_RTU_FRAME_DATA_LENGTH - 2;
-        &self.data[2..END_INDEX]
+        &self.data[1..END_INDEX]
     }
 
     fn function_code(&self) -> FunctionKind {
-        FunctionKind::from(self.data[2])
+        FunctionKind::from(self.data[1])
     }
 
     fn length(&self) -> Option<u16> {
@@ -26,14 +39,12 @@ impl<'a> PduData for ModbusRtuFrame<'a> {
 impl<'a> PduDataMut for ModbusRtuFrame<'a> {
     fn pdu_data_mut(&mut self) -> &mut [u8] {
         const END_INDEX: usize = MODBUS_RTU_FRAME_DATA_LENGTH - 2;
-        &mut self.data[2..END_INDEX]
+        &mut self.data[1..END_INDEX]
     }
 
     fn set_function_code(&mut self, code: FunctionKind) {
-        self.data[2] = u8::from(code);
+        self.data[1] = u8::from(code);
     }
 
-    fn set_length(&mut self, _length: u16) {
-        
-    }
+    fn set_length(&mut self, _length: u16) {}
 }
