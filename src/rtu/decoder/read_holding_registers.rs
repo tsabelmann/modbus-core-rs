258e2dbd0fc39ae7f164d9de4d40183ac9f82613
tsabelmann <tim.sabelmann@hotmail.de>
2025-09-29 14:57:17 +0200

    init: Add read_holding_registers.rs

---
+++ src/rtu/decoder/read_holding_registers.rs
@@ -0,0 +1,16 @@
+
+
+// pub struct ReadHoldingRegistersRequestRtuFrameDecoder<'a> {
+//     data: &'a mut [u8]
+// }
+
+// impl<'a> ReadHoldingRegistersRequestRtuFrameDecoder {
+
+
+//     pub fn push_data(&mut self) {
+
+//     }
+
+// }
+
+
