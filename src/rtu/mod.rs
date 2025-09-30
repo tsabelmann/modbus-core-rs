7a6357526fef4269ea989cf9e0e1937339cf573b
tsabelmann <tim.sabelmann@hotmail.de>
2025-09-30 12:42:05 +0200

    feat: Add imports for submodule types

--- src/rtu/mod.rs
+++ src/rtu/mod.rs
@@ -1,7 +1,8 @@
 mod crc;
-pub use crc::rtu_crc;
+pub use crc::{rtu_crc, check_rtu_crc};
 
 mod decoder;
+pub use decoder::{ModbusRtuFrameRequestDecoder, ModbusRtuFrameRequestDecoderResult};
 
 mod frame;
 pub use frame::ModbusRtuFrame;
