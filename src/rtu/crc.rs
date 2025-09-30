b416c1fc22ab30cf0947d6d517aa92e033776da0
tsabelmann <tim.sabelmann@hotmail.de>
2025-09-30 12:41:00 +0200

    feat: Add additional method for checking the CRC

--- src/rtu/crc.rs
+++ src/rtu/crc.rs
@@ -15,3 +15,15 @@ pub fn rtu_crc(data: &[u8]) -> u16 {
 
     crc
 }
+
+/// Check the computed checksum against the transmitted/received checksum.
+pub fn check_rtu_crc(data: &[u8], crc_low: u8, crc_high: u8) -> bool {
+    let computed_crc = rtu_crc(data);
+    let bytes = computed_crc.to_le_bytes();
+
+    if bytes[0] == crc_low && bytes[1] == crc_high {
+        true
+    } else {
+        false
+    }
+}
