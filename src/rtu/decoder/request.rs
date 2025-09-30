21e6e5f67b0b37c54f6ee224e2faa8dc08466623
tsabelmann <tim.sabelmann@hotmail.de>
2025-09-30 12:49:32 +0200

    test: Add test cases for read coils and read holding registers requests

--- src/rtu/decoder/request.rs
+++ src/rtu/decoder/request.rs
@@ -3,6 +3,7 @@ use crate::{constants::MODBUS_RTU_FRAME_DATA_LENGTH, rtu::{crc::check_rtu_crc, M
 fn compute_pdu_data_length_for_request_frame(function_code: FunctionCode) -> usize {
     match function_code {
         FunctionCode::ReadHoldingRegisters => 4,
+        FunctionCode::ReadCoils => 4,
         _ => 0
     }
 }
@@ -136,10 +137,44 @@ impl<'a> ModbusRtuFrameRequestDecoder<'a> {
 mod rtu_frames_tests {
     use super::*;
 
-    #[test]
-    fn rtu_test_001() {
+     #[test]
+    fn rtu_frame_decoder_read_coils_001() {
         let mut buffer = [0u8; MODBUS_RTU_FRAME_DATA_LENGTH];
         let mut decoder = ModbusRtuFrameRequestDecoder::new(&mut buffer);
+
+        let _ = decoder.push_data(0x04); 
+        let _ = decoder.push_data(0x01);
+        let _ = decoder.push_data(0x00);
+        let _ = decoder.push_data(0x0A);
+        let _ = decoder.push_data(0x00);
+        let _ = decoder.push_data(0x0D);
+        let _ = decoder.push_data(0xDD);
+        match decoder.push_data(0x98) {
+            ModbusRtuFrameRequestDecoderResult::Success(_frame) => {
+                assert!(true);
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn rtu_frame_decoder_read_holding_registers_001() {
+        let mut buffer = [0u8; MODBUS_RTU_FRAME_DATA_LENGTH];
+        let mut decoder = ModbusRtuFrameRequestDecoder::new(&mut buffer);
+
+        let _ = decoder.push_data(0x01); 
+        let _ = decoder.push_data(0x03);
+        let _ = decoder.push_data(0x00);
+        let _ = decoder.push_data(0x00);
+        let _ = decoder.push_data(0x00);
+        let _ = decoder.push_data(0x02);
+        let _ = decoder.push_data(0xC4);
+        match decoder.push_data(0x0B) {
+            ModbusRtuFrameRequestDecoderResult::Success(_frame) => {
+                assert!(true);
+            },
+            _ => assert!(false)
+        }
 
         let _ = decoder.push_data(0x01); 
         let _ = decoder.push_data(0x03);
