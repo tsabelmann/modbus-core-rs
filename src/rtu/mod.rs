4d4fc33f1286ee21ca49298e9d909085aea48649
tsabelmann <tim.sabelmann@hotmail.de>
2025-10-08 10:48:57 +0200

    refact: Change import names

--- src/rtu/mod.rs
+++ src/rtu/mod.rs
@@ -2,7 +2,7 @@ mod crc;
 pub use crc::{rtu_crc, check_rtu_crc};
 
 mod decoder;
-pub use decoder::{ModbusRtuFrameRequestDecoder, ModbusRtuFrameRequestDecoderResult};
+pub use decoder::{ModbusRtuFrameDecoder, ModbusRtuFrameDecoderResult};
 
 mod frame;
 pub use frame::ModbusRtuFrame;
