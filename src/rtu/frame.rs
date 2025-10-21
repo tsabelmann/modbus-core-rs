c470a606d4c7733cfd8b1a8f144fef0602a9037e
tsabelmann <tim.sabelmann@hotmail.de>
2025-10-21 13:15:25 +0200

    feat: Add slave_address_mut method

--- src/rtu/frame.rs
+++ src/rtu/frame.rs
@@ -18,8 +18,8 @@ impl<'a> ModbusRtuFrame<'a> {
         self.data[0]
     }
 
-    pub fn slave_address_mut(&mut self, slave_address: u8) {
-        self.data[0] = slave_address;
+    pub fn slave_address_mut(&mut self) -> &mut u8 {
+        &mut self.data[0]
     }
 
     pub fn checksum(&self) -> u16 {
