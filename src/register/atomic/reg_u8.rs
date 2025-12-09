bbd3332cbadfb339b9eeba9d2ed36321b25ed619
tsabelmann <tim.sabelmann@hotmail.de>
2025-12-09 13:48:08 +0100

    feat: Implement skeleton for RegAtomicU8 and RegAtomicU8Ref

--- src/register/atomic/reg_u8.rs
+++ src/register/atomic/reg_u8.rs
@@ -1,6 +1,16 @@
 use core::sync::atomic::{AtomicU8, Ordering};
 use crate::register::{RegisterData, ReadRegisterData, WriteRegisterData, RegisterResult, RegisterError};
 
+pub struct RegAtomicU8 {
+    value: AtomicU8
+}
+
+impl<'a> RegAtomicU8 {
+    pub const fn new(value: u8) -> RegAtomicU8 {
+        RegAtomicU8 { value: AtomicU8::new(value) }
+    }
+}
+
 pub struct RegAtomicU8Ref<'a> {
     value: &'a AtomicU8
 }
@@ -13,11 +23,22 @@ impl<'a> RegAtomicU8Ref<'a> {
 
 /* RegisterData */
 
+impl RegisterData for RegAtomicU8 {
+    const COUNT: usize = 1;
+}
+
 impl<'a> RegisterData for RegAtomicU8Ref<'a> {
     const COUNT: usize = 1;
 }
 
 /* ReadRegisterData */
+
+impl ReadRegisterData for RegAtomicU8 {
+    fn read(&self, offset: usize, buf: &mut [u16]) -> RegisterResult<usize> {
+        let reff = RegAtomicU8Ref::new(&self.value);
+        reff.read(offset, buf)
+    }
+}
 
 impl<'a> ReadRegisterData for RegAtomicU8Ref<'a> {
     fn read(&self, offset: usize, buf: &mut [u16]) -> RegisterResult<usize> {
@@ -32,11 +53,17 @@ impl<'a> ReadRegisterData for RegAtomicU8Ref<'a> {
 
         buf[..to_read].copy_from_slice(&data[offset..offset + to_read]);
         Ok(to_read)
-
     }
 }
 
 /* WriteRegisterData */
+
+impl WriteRegisterData for RegAtomicU8 {
+    fn write(&mut self, offset: usize, buf: &[u16]) -> RegisterResult<usize> {
+        let mut reff = RegAtomicU8Ref::new(&self.value);
+        reff.write(offset, buf)
+    }
+}
 
 impl<'a> WriteRegisterData for RegAtomicU8Ref<'a> {
     fn write(&mut self, offset: usize, buf: &[u16]) -> RegisterResult<usize> {
