c3806b03da62aeac42d020c683610b1bfec7b07f
tsabelmann <tim.sabelmann@hotmail.de>
2026-04-17 16:54:48 +0200

    feat: Change name to Request, and RequestError

--- src/decode/request/write_single_register.rs
+++ src/decode/request/write_single_register.rs
@@ -1,74 +1,52 @@
 use crate::{FunctionCode, FunctionKind, PduData};
 
-pub struct WriteSingleRegisterRequestDecoder<'a, T> 
-where 
-    T: PduData
-{
-    pdu: &'a T
-}
+// ── DecoderError ──
 
 #[non_exhaustive]
 #[repr(u8)]
 #[derive(Debug, PartialEq, Clone, Copy)]
-pub enum WriteSingleRegisterRequestDecoderError {
+pub enum RequestError {
     NotEnoughData,
     InvalidFunctionCode
 }
 
-impl<'a, T: PduData> WriteSingleRegisterRequestDecoder<'a, T> {
-    pub fn new(pdu: &'a T) -> Result<WriteSingleRegisterRequestDecoder<'a, T>, WriteSingleRegisterRequestDecoderError> {
-        let reqired_length = 5;
-        if pdu.pdu_data().len() < reqired_length {
-            Err(WriteSingleRegisterRequestDecoderError::NotEnoughData)
-        } else {
-            let code = pdu.function_code();
-            match code {
-                FunctionKind::Normal(FunctionCode::WriteSingleRegister) => {
-                    Ok(WriteSingleRegisterRequestDecoder { pdu })
-                }
-                _ => Err(WriteSingleRegisterRequestDecoderError::InvalidFunctionCode)
+// ── RequestDecoder ──
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct Request {
+    register_address: u16,
+    register_value: u16,
+}
+
+impl Request {
+    pub fn new(pdu: &dyn PduData) -> Result<Request, RequestError> {
+        if pdu.pdu_data().len() < 5 {
+            return Err(RequestError::NotEnoughData);
+        }
+
+        match pdu.function_code() {
+            FunctionKind::Normal(FunctionCode::WriteSingleRegister) => {
+                let register_address = u16::from_be_bytes(
+                    [pdu.pdu_data()[1], pdu.pdu_data()[2]]
+                );
+                let register_value = u16::from_be_bytes(
+                    [pdu.pdu_data()[3], pdu.pdu_data()[4]]
+                );
+                Ok(Request { register_address, register_value })
             }
+            _ => Err(RequestError::InvalidFunctionCode),
         }
     }
 
     pub fn function_code(&self) -> FunctionKind {
-        self.pdu.function_code()
+        FunctionKind::Normal(FunctionCode::WriteSingleRegister)
     }
 
     pub fn register_address(&self) -> u16 {
-        let data = [self.pdu.pdu_data()[1], self.pdu.pdu_data()[2]];
-        u16::from_be_bytes(data)
+        self.register_address
     }
 
     pub fn register_value(&self) -> u16 {
-        let data = [self.pdu.pdu_data()[3], self.pdu.pdu_data()[4]];
-        u16::from_be_bytes(data)
+        self.register_value
     }
 }
-
-pub struct WriteSingleRegisterRequestDecoderIter<'a, T: PduData> {
-    pdu: WriteSingleRegisterRequestDecoder<'a, T>,
-    index: u8
-}
-
-impl<'a, T: PduData> Iterator for WriteSingleRegisterRequestDecoderIter<'a, T> {
-    type Item = u16;
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.index {
-            0 => Some(self.pdu.register_value()),
-            _ => None
-        }
-    }
-}
-
-impl<'a, T: PduData> IntoIterator for WriteSingleRegisterRequestDecoder<'a, T> {
-    type Item = u16;
-    type IntoIter = WriteSingleRegisterRequestDecoderIter<'a, T>;
-    fn into_iter(self) -> Self::IntoIter {
-        WriteSingleRegisterRequestDecoderIter {
-            pdu: self, 
-            index: 0
-        }
-    }
-}
-
