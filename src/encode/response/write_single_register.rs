05617b8d3bb9035669c3da5d15f9f3b2019c8ed1
tsabelmann <tim.sabelmann@hotmail.de>
2026-04-20 08:29:58 +0200

    feat: Update write single register response encoder

--- src/encode/response/write_single_register.rs
+++ src/encode/response/write_single_register.rs
@@ -1,81 +1,77 @@
-use crate::{ExceptionCode, FunctionCode, FunctionKind, PduDataMut};
+use crate::{ExceptionCode, FunctionCode, FunctionKind};
+use crate::encode::FrameEncoder;
+
+// ── EncodeError ──
 
 #[repr(u8)]
 #[non_exhaustive]
 #[derive(Debug, PartialEq, Clone, Copy)]
-pub enum WriteSingleRegisterReponseEncodeError {
+pub enum EncodeError {
     NotEnoughData
 }
 
-#[repr(u8)]
-#[non_exhaustive]
+// ── Response ──
+
 #[derive(Debug, PartialEq, Clone, Copy)]
-pub enum WriteSingleRegisterReponseEncodeExceptionError {
-    NotEnoughData,
-    InvalidExceptionCode
+pub enum Response {
+    Normal {
+        register_address: u16,
+        register_value: u16
+    },
+    Exception(ExceptionCode)
 }
 
-pub trait WriteSingleRegisterReponseEncoder {
-    fn encode(&mut self, register_address: u16, register_value: u16) -> Result<(), WriteSingleRegisterReponseEncodeError>; 
-    fn encode_exception(&mut self, code: ExceptionCode) -> Result<(), WriteSingleRegisterReponseEncodeExceptionError>;
-}
 
-impl<T: PduDataMut> WriteSingleRegisterReponseEncoder for T {
-    fn encode(&mut self, register_address: u16, register_value: u16) -> Result<(), WriteSingleRegisterReponseEncodeError> {
-        // check for size  
-        let required_length = 5;
-        if self.pdu_data_mut().len() < required_length {
-            return Err(WriteSingleRegisterReponseEncodeError::NotEnoughData);   
-        }
+impl Response {
+    pub const MAX_PAYLOAD_LENGTH: usize = 4;
 
-        // write function code
-        self.set_function_code(FunctionKind::Normal(FunctionCode::WriteSingleRegister));
-
-        // write register address
-        let data = register_address.to_be_bytes();
-        self.pdu_data_mut()[1] = data[0];
-        self.pdu_data_mut()[2] = data[1];
-        
-        // write register value
-        let data = register_value.to_be_bytes();
-        self.pdu_data_mut()[3] = data[0];
-        self.pdu_data_mut()[4] = data[1];
-
-        // set length (necessary for TCP/IP based modbus frames)
-        let length = (1 + 5) as u16;
-        self.set_length(length);
-
-        // return Ok
-        Ok(())
+    pub const fn new(register_address: u16, register_value: u16) -> Response {
+        Response::Normal { register_address, register_value }
     }
 
-    fn encode_exception(&mut self, code: ExceptionCode) -> Result<(), WriteSingleRegisterReponseEncodeExceptionError> {
-        // check the required data
-        let required_length = 2;
-        if self.pdu_data_mut().len() < required_length {
-            return Err(WriteSingleRegisterReponseEncodeExceptionError::NotEnoughData);
+    pub const fn new_exception(code: ExceptionCode) -> Response {
+        Response::Exception(code)
+    }
+
+    pub fn encode(&self, frame: &mut dyn FrameEncoder) -> Result<(), EncodeError> {
+        match self {
+            Response::Normal { register_address, register_value } => {
+                let payload = frame.payload_mut();
+                if payload.len() < Self::MAX_PAYLOAD_LENGTH {
+                    return Err(EncodeError::NotEnoughData);
+                }
+
+                // register address
+                let bytes = register_address.to_be_bytes();
+                payload[0] = bytes[0];
+                payload[1] = bytes[1];
+
+                // register value
+                let bytes = register_value.to_be_bytes();
+                payload[2] = bytes[0];
+                payload[3] = bytes[1];
+
+                frame.finalize(
+                    FunctionKind::Normal(FunctionCode::WriteSingleRegister),
+                    Self::MAX_PAYLOAD_LENGTH,
+                );
+                Ok(())
+            },
+            Response::Exception(code) => {
+                let payload = frame.payload_mut();
+                if payload.is_empty() {
+                    return Err(EncodeError::NotEnoughData);
+                }
+
+                let raw = u8::from(*code);
+                payload[0] = raw;
+
+                frame.finalize(
+                    FunctionKind::Exception(FunctionCode::WriteSingleRegister),
+                    1,
+                );
+                Ok(())
+            },
         }
-
-        // set function code
-        self.set_function_code(FunctionKind::new_exception(FunctionCode::ReadHoldingRegisters));
-
-        // compute exception code
-        let exception_code = match code {
-            ExceptionCode::IllegalFunction => u8::from(code),
-            ExceptionCode::IllegalDataAddress => u8::from(code),
-            ExceptionCode::IllegalDataValue => u8::from(code),
-            ExceptionCode::ServerDeviceFailure => u8::from(code),
-            _ => return Err(WriteSingleRegisterReponseEncodeExceptionError::InvalidExceptionCode)
-        };
-
-        // set exception code
-        self.pdu_data_mut()[1] = exception_code;
-
-        // set length (necessary for TCP/IP based modbus frames)
-        let length = (1 + required_length)  as u16;
-        self.set_length(length);
-
-        // return Ok
-        Ok(())
     }
 }
